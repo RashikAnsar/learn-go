2cce43f908d87319815f7649ae8d00ace53905ac
RashikAnsar <sk.ansar46@gmail.com>
2018-09-05 13:07:09 +0530

    arrays declaration updated

--- 3.variables/3.arrays/var_arrays.go
+++ 3.variables/3.arrays/var_arrays.go
@@ -8,12 +8,17 @@ func main() {
 	vals[0] = 2
 	vals[1] = 4
 	vals[2] = 6
-
 	fmt.Println(vals)
+
+	x := [5]int{1, 2, 3, 4, 5}
+	fmt.Println(x)
 
 	var a [2]string
 	a[0] = "Hello"
 	a[1] = "World"
 	fmt.Println(a)
+
+	b := [2]string{"firstName", "lastName"}
+	fmt.Println(b)
 
 }
